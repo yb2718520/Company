a05fd51e55fa1422a3e30721ae9b4689ce27ad93
Jean <moongill2008@gmai.com>
2019-07-14 19:48:17 +0900

    一旦

--- marryJP/src/app/places/offers/detail/detail.page.ts
+++ marryJP/src/app/places/offers/detail/detail.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NavController } from '@ionic/angular';
+import { OffersService } from '../offers.service';
+import { CommunityEntity } from 'src/app/models/community.modal';
 
 @Component({
   selector: 'app-detail',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailPage implements OnInit {
 
-  constructor() { }
+  communityDetail: CommunityEntity;
+
+  constructor(
+    private navCtrl: NavController,
+    private route: ActivatedRoute,
+    private offersService: OffersService
+    ) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(param => {
+      if (!param.has('forumId')) {
+        this.navCtrl.navigateBack('/places/tabs/forum');
+        return;
+      }
+
+      console.log('id: ' + param.get('forumId'));
+      this.communityDetail = this.offersService.getInfoById(param.get('forumId'));
+    });
   }
 
 }
